587ea8ef62754e5d2bd24a0541c4360c829f3ecd
модуль4.1 <egorik100k@mail.ru>
2023-08-21 02:42:14 +0300

    PythonProject(DJango)

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+    # небольшое текстовое поле
+    title = models.CharField("Заголовок", max_length=128)
+
+    # описание
+    # большое текстовое поле
+    description = models.TextField("Описание")
+
+    # цена
+    # число с фиксированной точностью
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+
+    # уместен ли торг
+    # логическое значение
+    auction = models.BooleanField("Торг", help_text="Отметьте, если хотите торговаться")
+
+    # дата создания
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # дата изменения
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = 'advertisements'
+    # изображение
+
+    # адрес
+
+    # рейтинг продавца
+
+    # похожие товары
+
+    # кантакты продавца
